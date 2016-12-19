b876ee0e9a10a088e639233e2a1642ed436af29f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-12-19 17:45:28 +0100

    Implement 'TimeFormat'.

--- jpx/src/main/java/jpx/TimeFormat.java
+++ jpx/src/main/java/jpx/TimeFormat.java
@@ -19,12 +19,20 @@
  */
 package jpx;
 
+import static java.time.ZoneOffset.UTC;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 import static java.util.Objects.requireNonNull;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DecimalStyle;
+import java.time.format.ResolverStyle;
+import java.util.Optional;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
@@ -33,126 +41,204 @@ import java.util.regex.Pattern;
  */
 enum  TimeFormat {
 
-	/**
-	 * ISO-8601 time format: <i>HH:mm:ss.SSSZ</i>
-	 */
-	ISO_TIME_OFFSET(
-		"^\\d{2}:\\d{2}:\\d{2}.\\d{3}([+-]\\d{4}|Z)$",
-		"HH:mm:ss.SSSZ"
-	),
+	// http://books.xmlschemata.org/relaxng/ch19-77049.html
 
-	/**
-	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ss</i>
-	 */
+	// 2001-10-26T21:32:52
 	ISO_DATE_TIME(
 		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$",
-		"yyyy-MM-dd'T'HH:mm:ss"
+		new DateTimeFormatterBuilder()
+			.append(ISO_LOCAL_DATE_TIME)
+			.optionalStart()
+			.appendOffsetId()
+			.toFormatter()
+			.withZone(UTC)
 	),
 
-	/**
-	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ssZ</i>
-	 */
-	ISO_DATE_TIME_OFFSET(
-		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}([+-]\\d{4}|Z)$",
-		"yyyy-MM-dd'T'HH:mm:ssZ"
+	// 2001-10-26T21:32:52+02:00
+	ISO_DATE_TIME2(
+		"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}([+-]\\d{2}:\\d{2})",
+		new DateTimeFormatterBuilder()
+			.append(ISO_LOCAL_DATE_TIME)
+			.optionalStart()
+			.appendOffsetId()
+			.toFormatter()
 	),
 
-	/**
-	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ss'Z'</i>
-	 * UTC (Zulu) Timezone
-	 */
-	ISO_DATE_TIME_ZULU(
-		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$",
-		"yyyy-MM-dd'T'HH:mm:ss'Z'"
+	// 2001-10-26T21:32:52Z
+	ISO_DATE_TIME3(
+		"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z",
+			new DateTimeFormatterBuilder()
+			.append(ISO_LOCAL_DATE_TIME)
+			.optionalStart()
+			.appendOffsetId()
+			.toFormatter()
+			.withZone(UTC)
 	),
 
-	/**
-	 * ISO-8601 timestamp format: <i>yyyy-MM-dd'T'HH:mm:ss.SSS</i>
-	 */
-	ISO_TIMESTAMP(
-		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}$",
-		"yyyy-MM-dd'T'HH:mm:ss.SSS"
-	),
+	// 2001-10-26T21:32:52Z
+	ISO_DATE_TIME4(
+		"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{1,9}",
+			new DateTimeFormatterBuilder()
+			.append(ISO_LOCAL_DATE_TIME)
+			.optionalStart()
+			.appendOffsetId()
+			.toFormatter()
+			.withZone(UTC)
+	)
 
-	/**
-	 * ISO-8601 timestamp format: <i>yyyy-MM-dd'T'HH:mm:ss.SSSZ</i>
-	 */
+	;
+
+
+
+	/*
 	ISO_TIMESTAMP_OFFSET(
-		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}([+-]\\d{4}|Z)$",
-		"yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}([+-]\\d{4})$",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 	),
 
-	/**
-	 * Number date format only: <i>yyyMMdd</i>
-	 */
-	ISO_NUMERIC_DATE(
-		"^\\d{8}$",
-		"yyyyMMdd"
+
+
+	ISO_DATE_TIME2(
+		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{1}$",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.S")
 	),
 
-	/**
-	 * Number date time format only: <i>yyyMMddHHmmss</i>
-	 */
-	ISO_NUMERIC_DATE_TIME(
-		"^\\d{14}$",
-		"yyyyMMddHHmmss"
+	ISO_DATE_TIME3(
+		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{2}$",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SS")
 	),
 
-	JAVASCRIPT_DATE_TIME(
-		"^[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT[+-]{1}\\d{4}$",
-		"EEE MMM dd yyyy HH:mm:ss 'GMT'Z"
+	ISO_DATE_TIME4(
+		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}$",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
 	),
 
-	/**
-	 * Custom ISO date time format: <i>yyyy-MM-dd HH:mm:ss</i>
-	 */
-	CUSTOM_ISO_DATE_TIME(
-		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$",
-		"yyyy-MM-dd HH:mm:ss"
-	),
-
-	/**
-	 * Custom ISO date time format: <i>yyyy-MM-dd HH:mm:ss</i>
-	 */
-	CUSTOM_ISO_DATE_TIME_MILLIS(
-		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{3}$",
-		"yyyy-MM-dd HH:mm:ss.SSS"
-	),
-
-	/**
-	 * Custom ISO date time format with offset and without millis:
-	 * <i>yyyy-MM-dd HH:mm:ssZ</i>
-	 */
-	CUSTOM_ISO_DATE_TIME_OFFSET(
-		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}([+-]\\d{2,4}|Z)$",
-		"yyyy-MM-dd HH:mm:ssZ"
-	),
-
-	/**
-	 * Date time format with offset and without millis used in
-	 * Apache log files. E.g.: 26/Mar/2015:04:39:15 +0100
-	 * <i>dd/MMM/yyyy:HH:mm:ss Z</i>
-	 */
-	APACHE_LOG_DATE_TIME_OFFSET(
-		"^\\d{2}/[a-z,A-Z]{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} ([+-]\\d{2,4}|Z)$",
-		"dd/MMM/yyyy:HH:mm:ss Z"
-	),
-
-	/**
-	 * Date time format without offset and without time as used in EBAY user report.
-	 * <i>dd/MM/yyyy</i>
-	 */
-	UK_DATE(
-		"^\\d{2}/\\d{2}/\\d{4}$",
-		"dd/MM/yyyy"
+	ISO_TIMESTAMP_OFFSET2(
+		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}([+-]\\d{4}\\[*?\\])$",
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 	);
+	*/
+
+//	/**
+//	 * ISO-8601 time format: <i>HH:mm:ss.SSSZ</i>
+//	 */
+//	ISO_TIME_OFFSET(
+//		"^\\d{2}:\\d{2}:\\d{2}.\\d{3}([+-]\\d{4}|Z)$",
+//		"HH:mm:ss.SSSZ"
+//	),
+//
+//	/**
+//	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ss</i>
+//	 */
+//	ISO_DATE_TIME(
+//		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$",
+//		"yyyy-MM-dd'T'HH:mm:ss"
+//	),
+//
+//	/**
+//	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ssZ</i>
+//	 */
+//	ISO_DATE_TIME_OFFSET(
+//		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}([+-]\\d{4}|Z)$",
+//		"yyyy-MM-dd'T'HH:mm:ssZ"
+//	),
+//
+//	/**
+//	 * ISO-8601 date time format: <i>yyyy-MM-dd'T'HH:mm:ss'Z'</i>
+//	 * UTC (Zulu) Timezone
+//	 */
+//	ISO_DATE_TIME_ZULU(
+//		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$",
+//		"yyyy-MM-dd'T'HH:mm:ss'Z'"
+//	),
+//
+//	/**
+//	 * ISO-8601 timestamp format: <i>yyyy-MM-dd'T'HH:mm:ss.SSS</i>
+//	 */
+//	ISO_TIMESTAMP(
+//		"^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}$",
+//		"yyyy-MM-dd'T'HH:mm:ss.SSS"
+//	),
+//
+//	/**
+//	 * Number date format only: <i>yyyMMdd</i>
+//	 */
+//	ISO_NUMERIC_DATE(
+//		"^\\d{8}$",
+//		"yyyyMMdd"
+//	),
+//
+//	/**
+//	 * Number date time format only: <i>yyyMMddHHmmss</i>
+//	 */
+//	ISO_NUMERIC_DATE_TIME(
+//		"^\\d{14}$",
+//		"yyyyMMddHHmmss"
+//	),
+//
+//	JAVASCRIPT_DATE_TIME(
+//		"^[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT[+-]{1}\\d{4}$",
+//		"EEE MMM dd yyyy HH:mm:ss 'GMT'Z"
+//	),
+//
+//	/**
+//	 * Custom ISO date time format: <i>yyyy-MM-dd HH:mm:ss</i>
+//	 */
+//	CUSTOM_ISO_DATE_TIME(
+//		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$",
+//		"yyyy-MM-dd HH:mm:ss"
+//	),
+//
+//	/**
+//	 * Custom ISO date time format: <i>yyyy-MM-dd HH:mm:ss</i>
+//	 */
+//	CUSTOM_ISO_DATE_TIME_MILLIS(
+//		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{3}$",
+//		"yyyy-MM-dd HH:mm:ss.SSS"
+//	),
+//
+//	/**
+//	 * Custom ISO date time format with offset and without millis:
+//	 * <i>yyyy-MM-dd HH:mm:ssZ</i>
+//	 */
+//	CUSTOM_ISO_DATE_TIME_OFFSET(
+//		"^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}([+-]\\d{2,4}|Z)$",
+//		"yyyy-MM-dd HH:mm:ssZ"
+//	),
+//
+//	/**
+//	 * Date time format with offset and without millis used in
+//	 * Apache log files. E.g.: 26/Mar/2015:04:39:15 +0100
+//	 * <i>dd/MMM/yyyy:HH:mm:ss Z</i>
+//	 */
+//	APACHE_LOG_DATE_TIME_OFFSET(
+//		"^\\d{2}/[a-z,A-Z]{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} ([+-]\\d{2,4}|Z)$",
+//		"dd/MMM/yyyy:HH:mm:ss Z"
+//	),
+//
+//	/**
+//	 * Date time format without offset and without time as used in EBAY user report.
+//	 * <i>dd/MM/yyyy</i>
+//	 */
+//	UK_DATE(
+//		"^\\d{2}/\\d{2}/\\d{4}$",
+//		"dd/MM/yyyy"
+//	);
 
 	private final Pattern _pattern;
 	private final DateTimeFormatter _formatter;
 
-	private TimeFormat(final String pattern, final String format) {
-		_pattern = Pattern.compile(pattern);
-		_formatter = DateTimeFormatter.ofPattern(format);
+	private TimeFormat(final Pattern pattern, final DateTimeFormatter formatter) {
+		_pattern = requireNonNull(pattern);
+		_formatter = requireNonNull(formatter);
+	}
+
+	private TimeFormat(final String pattern, final DateTimeFormatter formatter) {
+		this(Pattern.compile(pattern), formatter);
+	}
+
+	private boolean matches(final String time) {
+		return _pattern.matcher(time).matches();
 	}
 
 	public String format(final ZonedDateTime time) {
@@ -161,6 +247,19 @@ enum  TimeFormat {
 
 	public ZonedDateTime parse(final String time) {
 		return ZonedDateTime.parse(time, _formatter.withZone(ZoneId.systemDefault()));
+	}
+
+	public static Optional<TimeFormat> findFormatFor(final String time) {
+		return Stream.of(values())
+			.filter(tf -> tf.matches(time))
+			.findFirst();
+	}
+
+	public static Optional<ZonedDateTime> parseOption(final String time) {
+		return Stream.of(values())
+			.filter(tf -> tf.matches(time))
+			.map(tf -> tf.parse(time))
+			.findFirst();
 	}
 
 }
--- jpx/src/test/java/jpx/TimeFormatTest.java
+++ jpx/src/test/java/jpx/TimeFormatTest.java
@@ -20,6 +20,7 @@
 package jpx;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.testng.annotations.DataProvider;
@@ -36,7 +37,7 @@ public class TimeFormatTest {
 		System.out.println(format.format(time));
 	}
 
-	//@Test(dataProvider = "formats")
+	@Test(dataProvider = "formats")
 	public void parse(final TimeFormat format) {
 		final ZonedDateTime time = ZonedDateTime.now();
 		final String string = format.format(time);
@@ -44,11 +45,44 @@ public class TimeFormatTest {
 		format.parse(string);
 	}
 
+	@Test(dataProvider = "validExamples")
+	public void parseExample(final String example) {
+		final Optional<TimeFormat> format = TimeFormat.findFormatFor(example);
+		format.ifPresent(f -> {
+			final ZonedDateTime time = f.parse(example);
+			System.out.println(example + " -> " + f.format(time));
+		});
+		if (!format.isPresent()) {
+			System.out.println(example + " -> ");
+		}
+	}
+
 	@DataProvider(name = "formats")
 	public Object[][] formats() {
 		return Stream.of(TimeFormat.values())
 			.map(format -> new Object[]{format})
 			.toArray(Object[][]::new);
+	}
+
+	@DataProvider(name = "validExamples")
+	public Object[][] validExamples() {
+		return new Object[][] {
+			{"2001-10-26T21:32:52"},
+			{"2001-10-26T21:32:52+02:00"},
+			{"2001-10-26T19:32:52Z"},
+			{"2001-10-26T19:32:52+00:00"},
+			{"-2001-10-26T21:32:52"},
+			{"2001-10-26T21:32:52.12679"}
+
+			/*
+			{"2002-05-30T09:00:00"},
+			{"2002-05-30T09:30:10.5"},
+			{"2002-05-30T09:30:10.53"},
+			{"2002-05-30T09:30:10.536"},
+			{"2016-12-19T15:30:50.485+0100"},
+			{"2016-12-19T15:30:50.485+01:00[Europe/Vienna]"}
+			*/
+		};
 	}
 
 }
