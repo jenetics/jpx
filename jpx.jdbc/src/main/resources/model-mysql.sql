6e2e0dfeeb421654cad1620439eb27d17c79ac51
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-12-21 22:52:35 +0100

    Implement MySQL creation script.

--- jpx.jdbc/src/main/resources/model-mysql.sql
+++ jpx.jdbc/src/main/resources/model-mysql.sql
@@ -25,7 +25,7 @@ CREATE TABLE copyright(
 );
 CREATE UNIQUE INDEX i_copyright_author ON copyright(author);
 
-CREATE TABLE bounce(
+CREATE TABLE bounds(
 	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	minlat DOUBLE PRECISION NOT NULL,
 	minlon DOUBLE PRECISION NOT NULL,
@@ -48,7 +48,6 @@ CREATE TABLE metadata(
 );
 CREATE UNIQUE INDEX i_metadata_name ON metadata(name);
 
-
 CREATE TABLE metadata_link(
 	metadata_id BIGINT NOT NULL,
 	link_id BIGINT NOT NULL,
@@ -58,14 +57,138 @@ CREATE TABLE metadata_link(
 );
 CREATE UNIQUE INDEX i_metadata_id_link_id ON metadata_link(metadata_id, link_id);
 
+CREATE TABLE way_point(
+	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	lat DOUBLE PRECISION NOT NULL,
+	lon DOUBLE PRECISION NOT NULL,
+	ele DOUBLE PRECISION,
+	speed DOUBLE PRECISION,
+	time TIMESTAMP,
+	magvar DOUBLE PRECISION,
+	geohight DOUBLE PRECISION,
+	name VARCHAR(255),
+	cmt VARCHAR(255),
+	`desc` TEXT,
+	src VARCHAR(255),
+	sym VARCHAR(255),
+	type VARCHAR(255),
+	fix VARCHAR(10),
+	sat INT,
+	hdop DOUBLE PRECISION,
+	vdop DOUBLE PRECISION,
+	pdop DOUBLE PRECISION,
+	ageofdgpsdata INT,
+	dgpsid INT
+);
+CREATE INDEX i_way_point_lat ON way_point(lat);
+CREATE INDEX i_way_point_lon ON way_point(lon);
+
+CREATE TABLE way_point_link(
+	way_point_id BIGINT NOT NULL,
+	link_id BIGINT NOT NULL,
+
+	FOREIGN KEY (way_point_id) REFERENCES way_point(id),
+	FOREIGN KEY (link_id) REFERENCES link(id)
+);
+CREATE UNIQUE INDEX i_way_point_link_id_link_id ON way_point_link(way_point_id, link_id);
+
+CREATE TABLE route(
+	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255),
+	cmt VARCHAR(255),
+	`desc` TEXT,
+	src VARCHAR(255),
+	number INT,
+	type VARCHAR(255)
+);
+
+CREATE TABLE route_link(
+	route_id BIGINT NOT NULL,
+	link_id BIGINT NOT NULL,
+
+	FOREIGN KEY (route_id) REFERENCES route(id),
+	FOREIGN KEY (link_id) REFERENCES link(id)
+);
+CREATE UNIQUE INDEX i_route_link_id_link_id ON route_link(route_id, link_id);
+
+CREATE TABLE route_way_point(
+	route_id BIGINT NOT NULL,
+	way_point_id BIGINT NOT NULL,
+
+	FOREIGN KEY (route_id) REFERENCES route(id),
+	FOREIGN KEY (way_point_id) REFERENCES way_point(id)
+);
+CREATE UNIQUE INDEX i_route_way_point_id_link_id ON route_way_point(route_id, way_point_id);
+
+CREATE TABLE track_segment(
+	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE track_segment_way_point(
+	track_segment_id BIGINT NOT NULL,
+	way_point_id BIGINT NOT NULL,
+
+	FOREIGN KEY (track_segment_id) REFERENCES track_segment(id),
+	FOREIGN KEY (way_point_id) REFERENCES way_point(id)
+);
+CREATE UNIQUE INDEX i_track_segment_way_point_id_link_id
+	ON track_segment_way_point(track_segment_id, way_point_id);
+
+
+CREATE TABLE track(
+	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255),
+	cmt VARCHAR(255),
+	`desc` TEXT,
+	src VARCHAR(255),
+	number INT,
+	type VARCHAR(255)
+);
+
+CREATE TABLE track_track_segment(
+	track_id BIGINT NOT NULL,
+	track_segment_id BIGINT NOT NULL,
+
+	FOREIGN KEY (track_id) REFERENCES track(id),
+	FOREIGN KEY (track_segment_id) REFERENCES track_segment(id)
+);
+CREATE UNIQUE INDEX i_track_segment_way_point_id_link_id
+	ON track_segment_way_point(track_segment_id, way_point_id);
+
+CREATE TABLE track_link(
+	track_id BIGINT NOT NULL,
+	link_id BIGINT NOT NULL,
+
+	FOREIGN KEY (track_id) REFERENCES track(id),
+	FOREIGN KEY (link_id) REFERENCES link(id)
+);
+CREATE UNIQUE INDEX i_track_link_id_link_id ON track_link(track_id, link_id);
+
 CREATE TABLE gpx(
 	version VARCHAR(5) NOT NULL DEFAULT '1.1',
-	creator VARCHAR(255) NOT NULL
-	-- private final Metadata _metadata;
-	-- private final List<WayPoint> _wayPoints;;
-	-- private final List<Route> _routes;
-	-- private final List<Track> _tracks;
+	creator VARCHAR(255) NOT NULL,
+	metadata_id BIGINT,
+
+	FOREIGN KEY (metadata_id) REFERENCES metadata(id)
 );
+
+CREATE TABLE gpx_way_point(
+	gpx_id BIGINT NOT NULL,
+	way_point_id BIGINT NOT NULL,
+
+	FOREIGN KEY (gpx_id) REFERENCES gpx(id),
+	FOREIGN KEY (way_point_id) REFERENCES way_point(id)
+);
+CREATE UNIQUE INDEX i_gpx_way_point_id_link_id ON gpx_way_point(gpx_id, way_point_id);
+
+CREATE TABLE gpx_track(
+	gpx_id BIGINT NOT NULL,
+	track_id BIGINT NOT NULL,
+
+	FOREIGN KEY (gpx_id) REFERENCES gpx(id),
+	FOREIGN KEY (track_id) REFERENCES track(id)
+);
+CREATE UNIQUE INDEX i_gpx_track_gpx_id_track_id ON gpx_track(gpx_id, track_id);
 
 
 
