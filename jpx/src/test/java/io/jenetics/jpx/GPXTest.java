d6521ddcb96dbdcf7306a890dcaa017601863e65
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-12-22 22:45:03 +0100

    Fix test.

--- jpx/src/test/java/io/jenetics/jpx/GPXTest.java
+++ jpx/src/test/java/io/jenetics/jpx/GPXTest.java
@@ -21,16 +21,12 @@ package io.jenetics.jpx;
 
 import static java.lang.String.format;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
-import java.util.stream.Stream;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import io.jenetics.jpx.geom.Geoid;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
@@ -70,70 +66,6 @@ public class GPXTest extends XMLStreamTestBase<GPX> {
 			.flatMap(TrackSegment::points)
 			.count();
 		Assert.assertEquals(length, 2747);
-	}
-
-	@Test
-	public void usage() throws Exception {
-		final GPX gpx = GPX.builder()
-			.addRoute(route -> route
-				.addPoint(p -> p.lat(48.2081743).lon(16.3738189).ele(160))
-				.addPoint(p -> p.lat(48.2081743).lon(16.3738189).ele(161)))
-			.addTrack(track -> track
-				.addSegment(segment -> segment
-					.addPoint(p -> p.lat(48.2081743).lon(16.3738189).ele(160))
-					.addPoint(p -> p.lat(48.2081743).lon(16.3738189).ele(161))
-					.addPoint(p -> p.lat(48.2081743).lon(16.3738189).ele(162))))
-			.build();
-
-		GPX.write(gpx, "    ", System.out);
-
-
-		final GPX gpx2 = GPX.builder()
-			.metadata(m -> m.author("Franz Wilhelmstötter"))
-			.addWayPoint(p -> p.lat(23.6).lon(13.5).ele(50))
-			.addRoute(route -> route.name("route-1")
-				.addPoint(p -> p.lon(12).lat(32).ele(12))
-				.addPoint(p -> p.lon(12).lat(32).ele(13))
-				.addPoint(p -> p.lon(12).lat(32).ele(14))
-				.addPoint(p -> p.lon(12).lat(32).ele(15))
-				.addPoint(p -> p.lon(12).lat(32).ele(16)))
-			.addTrack(track -> track.name("track-1")
-				.addSegment(segment -> segment
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12)))
-				.addSegment(segment -> segment
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))))
-			.addTrack(track -> track.name("track-2")
-				.addSegment(segment -> segment
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12)))
-				.addSegment(segment -> segment
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))
-					.addPoint(p -> p.lon(12).lat(32).ele(12))))
-			.build();
-
-		try (FileOutputStream out = new FileOutputStream("/home/fwilhelm/gpx.xml")) {
-			GPX.write(gpx, out);
-		}
-
-		GPX.write(gpx, "/home/fwilhelm/gpx.xml");
-		GPX gpx3 = GPX.read("/home/fwilhelm/gpx.xml");
-		final Length length = gpx3.tracks()
-			.flatMap(Track::segments)
-			.flatMap(TrackSegment::points)
-			.collect(Geoid.WGSC_84.toPathLength());
-
-		gpx3.tracks()
-			.flatMap(Track::segments)
-			.findFirst()
-			.map(TrackSegment::points).orElse(Stream.empty())
-			.collect(Geoid.WGSC_84.toPathLength());
-
 	}
 
 }
