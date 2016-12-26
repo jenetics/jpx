d51d15911c7708f56101a0661a3ad73d87cb0218
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-12-26 23:04:02 +0100

    Add additional value object tests.
    Testing 'equals' and 'hashCode' methods.

---
+++ jpx/src/test/java/io/jenetics/jpx/DegreesTest.java
@@ -0,0 +1,65 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package io.jenetics.jpx;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ */
+@Test
+public class DegreesTest extends ObjectTester<Degrees> {
+
+	@Override
+	Supplier<Degrees> factory(final Random random) {
+		return () -> Degrees.ofDegrees(random.nextDouble());
+	}
+
+	@Test
+	public void ofRadians() {
+		Assert.assertEquals(
+			Degrees.ofRadians(3),
+			Degrees.ofDegrees(Math.toDegrees(3))
+		);
+
+		Assert.assertEquals(
+			Degrees.ofRadians(3).toRadians(),
+			Degrees.ofDegrees(Math.toDegrees(3)).toRadians()
+		);
+	}
+
+	@Test
+	public void ofDegrees() {
+		Assert.assertEquals(
+			Degrees.ofDegrees(3),
+			Degrees.ofRadians(Math.toRadians(3))
+		);
+
+		Assert.assertEquals(
+			Degrees.ofDegrees(3).toDegrees(),
+			Degrees.ofRadians(Math.toRadians(3)).toDegrees()
+		);
+	}
+
+}
