5090df73b536e2cb23d08766fda89464b353b733
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-01-04 23:23:53 +0100

    #3: Implement way-point filter.

--- jpx/src/main/java/io/jenetics/jpx/filter/Merge.java
+++ jpx/src/main/java/io/jenetics/jpx/filter/Merge.java
@@ -1,5 +1,5 @@
 /*
- * Java Genetic Algorithm Library (@__identifier__@).
+ * Java GPX Library (@__identifier__@).
  * Copyright (c) @__year__@ Franz Wilhelmstötter
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,14 +15,69 @@
  * limitations under the License.
  *
  * Author:
- *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
  */
 package io.jenetics.jpx.filter;
 
+import static java.util.Collections.unmodifiableList;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.jenetics.jpx.GPX;
+import io.jenetics.jpx.Route;
+import io.jenetics.jpx.Track;
+import io.jenetics.jpx.TrackSegment;
+import io.jenetics.jpx.WayPoint;
+
 /**
- * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
  */
 public class Merge {
+
+	private Merge() {
+	}
+
+	public static GPX merge(final GPX gpx) {
+		return null;
+	}
+
+	public static Route mergeRoutes(final List<Route> routes) {
+		final List<WayPoint> points = routes.stream()
+			.flatMap(Route::points)
+			.collect(Collectors.toList());
+
+		final Route.Builder builder = routes.isEmpty()
+			? Route.builder()
+			: routes.get(0).toBuilder();
+
+		return builder
+			.points(unmodifiableList(points))
+			.build();
+	}
+
+	public static Track mergeTracks(final List<Track> tracks) {
+		final List<TrackSegment> segments = tracks.stream()
+			.flatMap(Track::segments)
+			.collect(Collectors.toList());
+
+		final Track.Builder builder = tracks.isEmpty()
+			? Track.builder()
+			: tracks.get(0).toBuilder();
+
+		return builder
+			.segments(unmodifiableList(segments))
+			.build();
+	}
+
+	public static TrackSegment mergeTrackSegments(final List<TrackSegment> segments) {
+		final List<WayPoint> points = segments.stream()
+			.flatMap(TrackSegment::points)
+			.collect(Collectors.toList());
+
+		return TrackSegment.of(unmodifiableList(points));
+	}
+
 }
