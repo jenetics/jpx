6c58bae869e0855c7f1c07b77e4b125a76945bdb
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2018-08-22 19:04:56 +0200

    #70: Remove outdated test dependency.

--- jpx/src/test/java/io/jenetics/jpx/format/LocationFormatterTest.java
+++ jpx/src/test/java/io/jenetics/jpx/format/LocationFormatterTest.java
@@ -19,21 +19,12 @@
  */
 package io.jenetics.jpx.format;
 
-import static java.lang.String.format;
-
-import us.fatehi.pointlocation6709.Angle;
-import us.fatehi.pointlocation6709.format.FormatterException;
-import us.fatehi.pointlocation6709.format.PointLocationFormatType;
-import us.fatehi.pointlocation6709.format.PointLocationFormatter;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -154,79 +145,4 @@ public class LocationFormatterTest {
 			.collect(Collectors.toList())
 			.iterator();
 	}
-
-
-
-	//@Test
-	public void latitude() throws FormatterException {
-		final Random random = new Random();
-
-		final Latitude latitude = LocationRandom.nextLatitude(random);
-		final us.fatehi.pointlocation6709.Latitude lat = toLat(latitude);
-
-		System.out.println(latitude);
-
-		for (PointLocationFormatType type : PointLocationFormatType.values()) {
-			try {
-				System.out.println(
-					type.toString() + ": " +
-						PointLocationFormatter.formatLatitude(lat, type)
-				);
-			} catch (Exception e) {
-				System.out.println("Not supported: " + type);
-			}
-		}
-	}
-
-	static us.fatehi.pointlocation6709.Latitude toLat(final Latitude latitude) {
-		final Angle angle = us.fatehi.pointlocation6709.Latitude
-			.fromDegrees(latitude.toDegrees());
-
-		return new us.fatehi.pointlocation6709.Latitude(angle);
-	}
-
-	public static void main(final String[] args) throws IOException {
-		final Random random = new Random(191929);
-		final List<Latitude> latitudes = Stream.generate(() -> LocationRandom.nextLatitude(random))
-			.limit(100)
-			.collect(Collectors.toList());
-
-		write(latitudes);
-	}
-
-	private static void write(final List<Latitude> latitudes) throws IOException {
-		final File baseDir = new File(BASE_DIR);
-		if (!baseDir.isDirectory() && !baseDir.mkdirs()) {
-			throw new IOException("Error while creating directory " + baseDir);
-		}
-
-		final StringBuilder out = new StringBuilder();
-		for (Latitude latitude : latitudes) {
-			out.append(String.format(
-				"%+17.15f\t%s\t%s\t%s\t%s\t%s\t%s\n",
-				latitude.toDegrees(),
-				format(latitude, PointLocationFormatType.HUMAN_LONG),
-				format(latitude, PointLocationFormatType.HUMAN_MEDIUM),
-				format(latitude, PointLocationFormatType.DECIMAL),
-				format(latitude, PointLocationFormatType.LONG),
-				format(latitude, PointLocationFormatType.MEDIUM),
-				format(latitude, PointLocationFormatType.SHORT)
-			));
-		}
-
-		final File file = new File(baseDir, "latitudes.csv");
-		Files.write(file.toPath(), out.toString().getBytes());
-	}
-
-	private static String format(
-		final Latitude latitude,
-		final PointLocationFormatType type
-	) {
-		try {
-			return PointLocationFormatter.formatLatitude(toLat(latitude), type);
-		} catch (FormatterException e) {
-			throw new AssertionError(e);
-		}
-	}
-
 }
