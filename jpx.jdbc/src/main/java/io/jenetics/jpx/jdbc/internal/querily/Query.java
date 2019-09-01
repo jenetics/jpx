43f886e0802c71880b0d6a01fde226830b34d2fd
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-09-01 20:04:30 +0200

    #1: Implement 'Query' interface.

--- jpx.jdbc/src/main/java/io/jenetics/jpx/jdbc/internal/querily/Query.java
+++ jpx.jdbc/src/main/java/io/jenetics/jpx/jdbc/internal/querily/Query.java
@@ -19,10 +19,85 @@
  */
 package io.jenetics.jpx.jdbc.internal.querily;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
+
 /**
+ * A {@code Query} represents an executable piece of SQL text.
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
  */
 public interface Query {
+
+	/**
+	 * Executes the SQL statement defined by {@code this} query object, which
+	 * may be any kind of SQL statement.
+	 *
+	 * @see PreparedStatement#execute()
+	 *
+	 * @param conn the DB connection where {@code this} query is executed on
+	 * @return {@code true} if the first result is a {@link java.sql.ResultSet}
+	 *         object; {@code false} if the first result is an update count or
+	 *         there is no result
+	 * @throws SQLException if a database access error occurs
+	 * @throws java.sql.SQLTimeoutException when the driver has determined that
+	 *         the timeout value has been exceeded
+	 * @throws NullPointerException if the given connection is {@code null}
+	 */
+	public boolean execute(final Connection conn) throws SQLException;
+
+	/**
+	 * Executes the SQL statement defined by {@code this} query object, which
+	 * must be an SQL Data Manipulation Language (DML) statement, such as
+	 * {@code INSERT}, {@code UPDATE} or {@code DELETE}; or an SQL statement
+	 * that returns nothing, such as a DDL statement.
+	 *
+	 * @see PreparedStatement#executeUpdate()
+	 *
+	 * @param conn the DB connection where {@code this} query is executed on
+	 * @return either (1) the row count for SQL Data Manipulation Language (DML)
+	 *         statements or (2) 0 for SQL statements that return nothing
+	 * @throws SQLException if a database access error occurs
+	 * @throws java.sql.SQLTimeoutException when the driver has determined that
+	 *         the timeout value has been exceeded
+	 * @throws NullPointerException if the given connection is {@code null}
+	 */
+	public int executeUpdate(final Connection conn) throws SQLException;
+
+	/**
+	 * Executes the SQL statement defined by {@code this} query object, which
+	 * must be an {@code INSERT} statement.
+	 *
+	 * @param conn the DB connection where {@code this} query is executed on
+	 * @return the key generated during the insertion
+	 * @throws SQLException if a database access error occurs
+	 * @throws java.sql.SQLTimeoutException when the driver has determined that
+	 *         the timeout value has been exceeded
+	 * @throws NullPointerException if the given connection is {@code null}
+	 */
+	public Optional<Long> executeInsert(final Connection conn)
+		throws SQLException;
+
+	/**
+	 * Executes {@code this} query and parses the result with the given
+	 * result-set parser.
+	 *
+	 * @param parser the parser which converts the query result to the desired
+	 *        type
+	 * @param conn the DB connection where {@code this} query is executed on
+	 * @param <T> the result type
+	 * @return the query result, parsed to the desired type
+	 * @throws SQLException if a database access error occurs
+	 * @throws java.sql.SQLTimeoutException when the driver has determined that
+	 *         the timeout value has been exceeded
+	 * @throws NullPointerException if the given result parser or connection is
+	 *         {@code null}
+	 */
+	public <T> T as(final ResultSetParser<T> parser, final Connection conn)
+		throws SQLException;
+
 }
