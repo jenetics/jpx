cb804ca8fc189c416cbb5edc0609cebead412fa4
Marcel <marcel.haas@hhu.de>
2019-10-15 16:01:28 +0200

    REMOVE: deprecated read methods

--- jpx/src/main/java/io/jenetics/jpx/GPX.java
+++ jpx/src/main/java/io/jenetics/jpx/GPX.java
@@ -1804,28 +1804,7 @@ public final class GPX implements Serializable {
 		return reader(Version.V11, Mode.STRICT).read(input);
 	}
 
-	/**
-	 * Read an GPX object from the given {@code input} stream.
-	 *
-	 * @param path the input path from where the GPX date is read
-	 * @param lenient if {@code true}, out-of-range and syntactical errors are
-	 *        ignored. E.g. a {@code WayPoint} with {@code lat} values not in
-	 *        the valid range of [-90..90] are ignored/skipped.
-	 * @return the GPX object read from the input stream
-	 * @throws IOException if the GPX object can't be read
-	 * @throws NullPointerException if the given {@code input} stream is
-	 *         {@code null}
-	 *
-	 * @see #reader(GPX.Version, GPX.Reader.Mode)
-	 * @deprecated Use {@code GPX.reader(Mode.LENIENT).read(path)} instead
-	 */
-	@Deprecated
-	public static GPX read(final Path path, final boolean lenient)
-		throws IOException
-	{
-		return reader(Version.V11, lenient ? Mode.LENIENT : Mode.STRICT)
-			.read(path);
-	}
+
 
 	/**
 	 * Read an GPX object from the given {@code input} stream. This method is a
@@ -1844,28 +1823,7 @@ public final class GPX implements Serializable {
 		return reader(Version.V11, Mode.STRICT).read(path);
 	}
 
-	/**
-	 * Read an GPX object from the given {@code input} stream.
-	 *
-	 * @param path the input path from where the GPX date is read
-	 * @param lenient if {@code true}, out-of-range and syntactical errors are
-	 *        ignored. E.g. a {@code WayPoint} with {@code lat} values not in
-	 *        the valid range of [-90..90] are ignored/skipped.
-	 * @return the GPX object read from the input stream
-	 * @throws IOException if the GPX object can't be read
-	 * @throws NullPointerException if the given {@code input} stream is
-	 *         {@code null}
-	 *
-	 * @see #reader(GPX.Version, GPX.Reader.Mode)
-	 * @deprecated Use {@code GPX.reader(lenient).read(path)} instead
-	 */
-	@Deprecated
-	public static GPX read(final String path, final boolean lenient)
-		throws IOException
-	{
-		return reader(Version.V11, lenient ? Mode.LENIENT : Mode.STRICT)
-			.read(path);
-	}
+
 
 	/**
 	 * Read an GPX object from the given {@code input} stream. This method is a
