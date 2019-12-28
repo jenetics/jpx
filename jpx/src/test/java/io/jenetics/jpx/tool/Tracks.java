adf6632db082642d0603cb6da65ae5c082b35f9d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-12-28 21:26:49 +0100

    Streamlining of 'Track' collector.

--- jpx/src/test/java/io/jenetics/jpx/tool/Tracks.java
+++ jpx/src/test/java/io/jenetics/jpx/tool/Tracks.java
@@ -3,9 +3,7 @@ package io.jenetics.jpx.tool;
 import static java.lang.String.format;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -16,53 +14,45 @@ import io.jenetics.jpx.Track;
 import io.jenetics.jpx.TrackSegment;
 import io.jenetics.jpx.WayPoint;
 
-final class Tracks {
+public final class Tracks {
 	private Tracks() {
 	}
 
+	/**
+	 * Return a new collector, which collects a given way point list into an
+	 * optional {@link Track}. All way points without a timestamp are filtered
+	 * out. A new segment is created if the timestamp of two consecutive points
+	 * are greater then the give {@code maxGap} duration. Each segment will
+	 * contain at least {@code minSegmentSize} points.
+	 *
+	 * @param maxGap the maximal allowed gap between two points within a
+	 *        track segment. If two points exceed the given gap, a new segment
+	 *        is created.
+	 * @param minSegmentSize the minimal number of way points a segment must
+	 *        consist
+	 * @return a new track segment collector
+	 * @throws NullPointerException if the given {@code maxGap} is {@code null}
+	 * @throws IllegalArgumentException if the {@code maxGap} or
+	 *         {@code minSegmentSize} is negative
+	 */
 	public static Collector<WayPoint, ?, Optional<Track>>
-	toTrack(final Duration gap, final int minSegmentSize) {
+	toTrack(final Duration maxGap, final int minSegmentSize) {
 		return Collectors.collectingAndThen(
-			Collectors.toList(),
-			points -> toTrack(points, gap, minSegmentSize)
+			TrackSegments.toTrackSegments(maxGap, minSegmentSize),
+			Tracks::toTrack
 		);
 	}
 
-	private static Optional<Track> toTrack(
-		final List<WayPoint> points,
-		final Duration gap,
-		final int minSegmentSize
-	) {
-		if (points.size() < minSegmentSize) {
+	private static Optional<Track> toTrack(final List<TrackSegment> segments) {
+		if (segments.isEmpty()) {
 			return Optional.empty();
 		}
 
 		final Track.Builder track = Track.builder()
 			.number(1)
 			.name("Track 1")
-			.cmt(trackCmt(points));
-
-		ZonedDateTime last = zonedDateTime(points.get(0));
-		TrackSegment.Builder segment = TrackSegment.builder();
-
-		for (final WayPoint point : points) {
-			final ZonedDateTime zdt = zonedDateTime(point);
-
-			if (last.plusNanos(gap.toNanos()).isAfter(zdt)) {
-				segment.addPoint(point);
-			} else {
-				if (segment.points().size() >= minSegmentSize) {
-					track.addSegment(segment.build());
-				}
-				segment = TrackSegment.builder();
-			}
-
-			last = zdt;
-		}
-
-		if (segment.points().size() >= minSegmentSize) {
-			track.addSegment(segment.build());
-		}
+			.cmt(trackCmt(segments))
+			.segments(segments);
 
 		track.desc(format(
 			"%d segments; %d track points",
@@ -72,25 +62,21 @@ final class Tracks {
 				.count()
 		));
 
-		return track.segments().isEmpty()
-			? Optional.empty()
-			: Optional.of(track.build());
+		return Optional.of(track.build());
 	}
 
-	private static ZonedDateTime zonedDateTime(final WayPoint wp) {
-		return wp
-			.getTime()
-			.orElse(ZonedDateTime.of(LocalDateTime.MAX, ZoneId.systemDefault()));
-	}
+	private static String trackCmt(final List<TrackSegment> segments) {
+		final List<WayPoint> points = segments.stream()
+			.flatMap(TrackSegment::points)
+			.collect(Collectors.toList());
 
-	private static String trackCmt(final List<WayPoint> points) {
 		final OffsetDateTime start = points.get(0).getTime()
 			.map(ZonedDateTime::toOffsetDateTime)
-			.orElse(OffsetDateTime.now());
+			.orElseThrow();
 
 		final OffsetDateTime end = points.get(points.size() - 1).getTime()
 			.map(ZonedDateTime::toOffsetDateTime)
-			.orElse(OffsetDateTime.now());
+			.orElseThrow();
 
 		return format(
 			"Track[start=%s, end=%s, duration=%s]",
