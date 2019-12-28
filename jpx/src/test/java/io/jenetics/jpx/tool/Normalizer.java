84932f3bb7c91d64eec1f55b94b08727d979673b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-12-28 15:41:09 +0100

    Splitting tracks.

--- jpx/src/test/java/io/jenetics/jpx/tool/Normalizer.java
+++ jpx/src/test/java/io/jenetics/jpx/tool/Normalizer.java
@@ -2,14 +2,22 @@ package io.jenetics.jpx.tool;
 
 import static java.lang.String.format;
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.groupingBy;
 import static io.jenetics.jpx.Bounds.toBounds;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import io.jenetics.jpx.Bounds;
@@ -20,8 +28,17 @@ import io.jenetics.jpx.Metadata;
 import io.jenetics.jpx.Person;
 import io.jenetics.jpx.Track;
 import io.jenetics.jpx.TrackSegment;
+import io.jenetics.jpx.WayPoint;
 
 public class Normalizer {
+
+	private static final
+	Collector<WayPoint, ?, Map<LocalDate, List<WayPoint>>>
+	TRACK_GROUPS = groupingBy(
+		p -> p.getTime()
+			.map(ZonedDateTime::toLocalDate)
+			.orElse(LocalDate.MIN)
+	);
 
 	public static void main(final String[] args) throws Exception {
 		if (args.length < 1) {
@@ -30,13 +47,22 @@ public class Normalizer {
 		}
 
 		//final Path file = Paths.get(args[0]).toAbsolutePath();
-		final Path file = Paths.get("/home/fwilhelm/Downloads/Ultra GPS Logger/2019-01-20_144714_Vienna.nmea.gpx");
+		final Path file = Paths.get("/home/fwilhelm/Downloads/raw-2018-05-02T130655.gpx");
 		System.out.println("Splitting " + file);
 
 		final GPX gpx = GPX
 			.reader(GPX.Version.V11, GPX.Reader.Mode.LENIENT)
 			.read(file);
 
+		final Map<LocalDate, List<WayPoint>> split = split(gpx);
+		final List<GPX> normalized = split.values().stream()
+			.flatMap(Normalizer::errorFilter)
+			.map(Normalizer::toGPX)
+			.collect(Collectors.toList());
+
+		write(file.getParent(), normalized);
+
+		/*
 		final GPX normalized = normalize(gpx);
 		final Path f = Paths.get(
 			file.getParent().toString(),
@@ -44,6 +70,51 @@ public class Normalizer {
 		);
 
 		GPX.writer("    ").write(normalized, f);
+		 */
+	}
+
+	private static void write(final Path dir, final List<GPX> gpxs)
+		throws IOException
+	{
+		for (GPX gpx : gpxs) {
+			if (gpx.getTracks().get(0).getSegments().isEmpty()) return;
+
+			final Path file = Paths.get(
+				dir.toString(),
+				fileName(gpx) + ".gpx"
+			);
+			System.out.println("Writing " + file);
+
+			GPX.writer("    ").write(gpx, file);
+		}
+	}
+
+	private static Map<LocalDate, List<WayPoint>> split(final GPX gpx) {
+		return gpx.tracks()
+			.flatMap(Track::segments)
+			.flatMap(TrackSegment::points)
+			.collect(TRACK_GROUPS);
+	}
+
+	private static Stream<List<WayPoint>> errorFilter(final List<WayPoint> points) {
+		final List<WayPoint> filtered = points.stream()
+			.filter(PointFilter.FAULTY_POINTS)
+			.collect(Collectors.toList());
+
+		return filtered.size() >= 10
+			? Stream.of(filtered)
+			: Stream.empty();
+	}
+
+	private static GPX toGPX(final List<WayPoint> points) {
+		final Track track = points.stream()
+			.collect(Tracks.toTrack(Duration.ofMinutes(5), 10));
+
+		return normalizeMetadata(
+			GPX.builder()
+				.tracks(singletonList(track))
+				.build()
+		);
 	}
 
 	private static GPX normalize(final GPX gpx) {
@@ -94,7 +165,7 @@ public class Normalizer {
 	private static String fileName(final GPX gpx)  {
 		return gpx.getMetadata()
 			.flatMap(Metadata::getTime)
-			.map(ZonedDateTime::toOffsetDateTime)
+			.map(ZonedDateTime::toLocalDate)
 			.map(Objects::toString)
 			.orElse("" + System.currentTimeMillis());
 	}
