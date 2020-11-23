3960eafc564ea2b2dc6d028f16a66ccee98815d4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-11-23 19:38:31 +0100

    #132: Update used library version.

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -23,19 +23,12 @@
  */
 object Libs {
 	const val ApacheCommonsMath = "org.apache.commons:commons-math3:3.6.1"
-	const val EqualsVerifier = "nl.jqno.equalsverifier:equalsverifier:3.3"
-	const val TestNG = "org.testng:testng:7.1.0"
-	const val JPX = "io.jenetics:jpx:2.0.0"
-	const val PRNGine = "io.jenetics:prngine:1.0.2"
-	const val RxJava = "io.reactivex.rxjava2:rxjava:2.1.14"
-	const val HsqlDB = "org.hsqldb:hsqldb:2.5.0"
+	const val EqualsVerifier = "nl.jqno.equalsverifier:equalsverifier:3.5"
+	const val TestNG = "org.testng:testng:7.3.0"
 
 	val All = arrayOf(
 		ApacheCommonsMath,
 		EqualsVerifier,
-		TestNG,
-		JPX,
-		PRNGine,
-		RxJava
+		TestNG
 	)
 }
