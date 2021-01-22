7d7c3872629735e02d7976f63180761cc888060d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-01-22 21:36:58 +0100

    Fix Javadoc warning.

--- jpx/src/main/java/io/jenetics/jpx/format/ParseException.java
+++ jpx/src/main/java/io/jenetics/jpx/format/ParseException.java
@@ -1,9 +1,40 @@
+/*
+ * Java GPX Library (@__identifier__@).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jenetics.jpx.format;
 
+/**
+ * This exception is thrown when the parsing of a location pattern string fails.
+ *
+ * @see LocationFormatter
+ */
 public class ParseException extends RuntimeException {
 	private static final long serialVersionUID = 1;
 
-	ParseException(String message, CharSequence in, int position){
+	/**
+	 * Create a new parse exception with the given parameters.
+	 *
+	 * @param message the error message
+	 * @param in the erroneous location string
+	 * @param position the error position
+	 */
+	ParseException(
+		final String message,
+		final CharSequence in,
+		final int position
+	) {
 		super(message + " at position " + position + " in " + in);
 	}
 
