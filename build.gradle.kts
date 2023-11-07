b48a91e7965ff2a10fa717b2fe6728fa6580b247
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-11-07 19:26:43 +0100

    #174: Update to Java 21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,8 +68,8 @@ gradle.projectsEvaluated {
 
 		plugins.withType<JavaPlugin> {
 			configure<JavaPluginExtension> {
-				sourceCompatibility = JavaVersion.VERSION_17
-				targetCompatibility = JavaVersion.VERSION_17
+				sourceCompatibility = JavaVersion.VERSION_21
+				targetCompatibility = JavaVersion.VERSION_21
 			}
 
 			configure<JavaPluginExtension> {
