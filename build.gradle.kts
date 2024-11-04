04df0a25c2b8395d59e4e6daae69bc6cabd1137d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2024-11-04 14:42:39 +0100

    #183: Update 'jacoco' version to 0.8.12.

--- build.gradle.kts
+++ build.gradle.kts
@@ -129,7 +129,7 @@ fun setupTestReporting(project: Project) {
 	project.apply(plugin = "jacoco")
 
 	project.configure<JacocoPluginExtension> {
-		toolVersion = "0.8.9"
+		toolVersion = "0.8.12"
 	}
 
 	project.tasks {
